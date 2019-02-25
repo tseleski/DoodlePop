b58adef7423752f9ca75dc5c32b3e1f414c305be
Tamar <tamarseleski@gmail.com>
2019-02-25 10:33:14 -0500

    Add hover to icons

--- src/bubble.js
+++ src/bubble.js
@@ -16,9 +16,6 @@ class Bubble {
   }
 
   draw(){
-    // var img = new Image();
-    // img.src = '../images/bubble.png';
-    // this.c.drawImage(img, 0, 0, 299, 299, this.x, this.y, this.radius, this.radius);
     this.c.beginPath();
     this.c.fillStyle = '#f9f8f7'; 
     this.c.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
@@ -27,14 +24,6 @@ class Bubble {
     this.c.strokeStyle = this.color;
     this.c.stroke();
   }
-
-  // draw(){
-  //   for (var i = 0; i < 500; i++) {
-  //     var radiusError = +10 - i / 20;
-  //     var d = 2 * Math.PI / 360 * i;
-  //     this.drawHelper(200 + 100 * Math.cos(d), 200 + (radiusError + 80) * Math.sin(d));
-  // }
-
 
   update(){
     if(Util.collidedWithChar(this, this.game.player)){
